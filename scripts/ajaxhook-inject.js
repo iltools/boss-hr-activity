e76c4f3a05afff384aa8fd08245200ebe6e62b32
iltools <15387673@qq.com>
2025-05-16 15:26:19 +0800

    feat: ajax拦截

---
+++ scripts/ajaxhook-inject.js
@@ -0,0 +1,16 @@
+// 拦截
+ah.proxy({
+  //请求发起前进入
+  onRequest: (config, handler) => {
+    handler.next(config);
+  },
+  //请求发生错误时进入，比如超时；注意，不包括http状态码错误，如404仍然会认为请求成功
+  onError: (err, handler) => {
+    handler.next(err);
+  },
+  //请求成功后进入
+  onResponse: (res, handler) => {
+    const { url, method } = res?.config || {};
+    console.log(url, '-----')
+  },
+});
